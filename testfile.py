7c9d0b9e31a79e335d8eee766393f95a07ef4b29
Minjoo Sur <loveshero92@gmail.com>
2017-08-12 16:05:21 -0700

    removed testfiles

--- testfile.py
+++
@@ -1,9 +0,0 @@
-tim = "timmi"
-minjoo = "mj"
-monty = "mt"
-mark = "mk"
-mark="mark"
-I wanna edit this
-If you see this on your master, it means this successfuly merged.
-I'm curious if other parts are deleted or stay.
-
