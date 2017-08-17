d641d0004d33764239bbbfd569c7a84b9d302b7a
muntadher inaya <msmontader@gmail.com>
2017-08-17 16:54:38 -0700

    configured aws DB, AWS S3

--- BackendAPI/BackendAPI/wsgi.py
+++ BackendAPI/BackendAPI/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BackendAPI.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BackendAPI.devprodsettings.prod")
 
 application = get_wsgi_application()
