cb2965ffd47d0c85e51d0ae677a437ce7ec182fd
Dolf Mardan <Dolf@Dolfs-MacBook-Pro-2.local>
2017-08-18 20:39:15 -0700

    login_signup page update by Dolf

---
+++ ios/ColoretApp/ColoretApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ColoretApp
+//
+//  Created by Dolf Mardan on 8/18/17.
+//  Copyright © 2017 Dolf Mardan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func login_google(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var login_google: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
