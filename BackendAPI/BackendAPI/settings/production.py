2306649aec41a33eddf2629201b07974e006831d
muntadher inaya <msmontader@gmail.com>
2017-08-19 14:51:50 -0700

    some modifications

--- BackendAPI/BackendAPI/settings/production.py
+++ BackendAPI/BackendAPI/settings/production.py
@@ -134,7 +134,6 @@ WSGI_APPLICATION = 'BackendAPI.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
 	    'default': {
 	        'ENGINE': 'django.db.backends.mysql',
